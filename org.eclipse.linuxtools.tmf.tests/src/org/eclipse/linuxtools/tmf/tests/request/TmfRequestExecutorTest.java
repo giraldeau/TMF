099fbdc11887ac701d2c745effcead713a3e05e8
fchouinar <fchouinar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-04-19 19:17:36 +0000

    [Bug309731] Fixed a problem with generic requests and component registration/deregistration.
    git-svn-id: http://dev.eclipse.org/svnroot/technology/org.eclipse.linuxtools/lttng/trunk@24603 ee007c2a-0a25-0410-9ab9-bf268980928c

---
+++ org.eclipse.linuxtools.tmf.tests/src/org/eclipse/linuxtools/tmf/tests/request/TmfRequestExecutorTest.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2010 Ericsson
+ * 
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Francois Chouinard - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.tmf.tests.request;
+
+import junit.framework.TestCase;
+
+/**
+ * <b><u>TmfRequestExecutorTest</u></b>
+ *
+ * Test suite for the TmfRequestExecutor class.
+ */
+public class TmfRequestExecutorTest extends TestCase {
+
+	/**
+	 * @param name
+	 */
+	public TmfRequestExecutorTest(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link org.eclipse.linuxtools.tmf.request.TmfRequestExecutor#TmfRequestExecutor(java.util.concurrent.ExecutorService)}.
+	 */
+	public void testTmfRequestExecutorExecutorService() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.eclipse.linuxtools.tmf.request.TmfRequestExecutor#TmfRequestExecutor()}.
+	 */
+	public void testTmfRequestExecutor() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.eclipse.linuxtools.tmf.request.TmfRequestExecutor#start()}.
+	 */
+	public void testStart() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.eclipse.linuxtools.tmf.request.TmfRequestExecutor#stop()}.
+	 */
+	public void testStop() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.eclipse.linuxtools.tmf.request.TmfRequestExecutor#execute(java.lang.Runnable)}.
+	 */
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.eclipse.linuxtools.tmf.request.TmfRequestExecutor#scheduleNext()}.
+	 */
+	public void testScheduleNext() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.eclipse.linuxtools.tmf.request.TmfRequestExecutor#queueRequest(java.lang.Runnable)}.
+	 */
+	public void testQueueRequest() {
+		fail("Not yet implemented");
+	}
+
+}
